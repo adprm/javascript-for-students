cefa3686f283320b0f912a2893b0be8cb790b280
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-13 13:25:54 +0700

    JavaScript Variable

---
+++ learn1.js
@@ -0,0 +1,14 @@
+// variable declaration
+var name = "Aditiya Permana";
+var visitor = 123;
+var status = true;
+var webAddress = "https://adprm.github.io";
+var country = "Indonesia"
+var cities = "Bogor";
+
+// alert("Welcome to course JavaScript " + name);
+console.log("Author name : " + name);
+console.log("Visitor : " + visitor);
+console.log("Url Address : " + webAddress);
+console.log("My country : " + country);
+console.log("My city : " + cities);
