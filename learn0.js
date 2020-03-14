b8e5ae04e98b11cb528c554c53f6ba751cb33a77
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-14 13:21:53 +0700

    JavaScript Comment

---
+++ learn0.js
@@ -0,0 +1,7 @@
+// Pendahuluan
+// 1 . ini aadalh komentar tidak akan di eksekusi
+/* ada juga yang seperti ini 
+untuk lebih dari 1 baris */
+console.log("Ini perintah untu dilihat hasil nya di console");
+document.write("Ini perintah untuk di lihat di halapan page web");
+alert("Ini perintah untuk menampilkan jendela di web");
