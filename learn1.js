3c9c7511e018e037802751af7cd818a35869f5d8
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 15:17:47 +0700

    JavaScript Variable

---
+++ learn1.js
@@ -0,0 +1,14 @@
+// JavaScript Varibale
+var name = "Aditiya Permana"; // Data type string
+var visitor = 123; // Data type number
+var status = true; // Data type boolean
+var webAddress = "https://adprm.github.io";
+var country = "Indonesia"
+var cities = "Bogor";
+
+// alert("Welcome to course JavaScript " + name);
+document.write("Author name : " + name + "<br>");
+document.write("Visitor : " + visitor + "<br>");
+document.write("Url Address : " + webAddress + "<br>");
+document.write("My country : " + country + "<br>");
+document.write("My city : " + cities + "<br>");
