e673b085fa2c616cad75c6ac644bb670b79f045e
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 15:24:05 +0700

    JavaScript Branching Ternary Operator

---
+++ learn7.js
@@ -0,0 +1,5 @@
+// JavaScript Branching Ternary Operator
+var ask = prompt("Apakah Jakarta ibu kota Indonesia ?");
+var answer = (ask.toUpperCase() == "IYA") ? "Benar" : "Salah";
+
+document.write(`Jawaban anda : <b> ${answer}`);
