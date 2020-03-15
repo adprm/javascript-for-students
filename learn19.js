0818d93c1509f095917d99f7d483b6fba54811d9
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 22:50:44 +0700

    Delete courses

--- learn19.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Get Data Array with Looping For
-var myHobbies = ['Futsal', 'Ngoding', 'Bernyanyi', 'Menari'];
-
-document.write("Hobi saya adalah : <br>")
-
-// get data array
-for (let i = 0; i < myHobbies.length; i++) {
-    document.write(i + ". " + myHobbies[i] + "<br>");
-}
--- learn20.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Get Data Array with Looping For Each
-var days = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'miggu'];
-
-// get data array
-days.forEach((days) => {
-    document.write("Mendapatkan data array dengan looping For Each <b>" + days + "</b><br>");
-});
