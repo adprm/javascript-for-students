b816e265026d29ced754609dd20ffa0b8b1be858
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 15:20:32 +0700

    JavaScript If Else

---
+++ learn4.js
@@ -0,0 +1,9 @@
+// JavaScript If Else
+var login = prompt("Masukkan password : ");
+if (login == "adit") {
+    document.write("password benar<br>");
+} else {
+    document.write("Password salah<br>")
+}
+
+document.write("Terimakasih")
