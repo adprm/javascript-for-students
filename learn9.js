adb06d9f141660a62efca337b9c82a446584e3a8
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 15:25:32 +0700

    JavaScript If and Operator Logica

---
+++ learn9.js
@@ -0,0 +1,9 @@
+// JavaScript If and Operator Logica
+var username = prompt("Masukkan username anda : ");
+var password = prompt("Masukkan password anda : ");
+
+if ((username.toUpperCase() == "ADIT") && (password.toUpperCase() == "TIDA")) {
+    document.write("Anda berhasl login")
+} else {
+    document.write("Input tidak valid. Login gagal")
+}
