5d24aa62d6fba76be61ebfa671c626806dca99a9
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 20:37:21 +0700

    JavaScript For Each and System Repeat

---
+++ learn14.js
@@ -0,0 +1,7 @@
+var days = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu'];
+
+days.forEach((days) => {
+    document.write(days + "<p>");
+});
+
+document.write("Menggunakan fungsi repeat hingga 50 <br>".repeat(50))
