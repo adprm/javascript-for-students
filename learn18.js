68df2a67893b442963a6ac57eb1f347c6cc25973
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 22:33:35 +0700

    JavaScript Get Data Array

---
+++ learn18.js
@@ -0,0 +1,5 @@
+// JavaScript Get Data Array
+var myFavoritFoods = ['Mie ayam', 'Bakso', 'Nasi Goreng', 'Gado-gado'];
+
+// Get data array
+document.write("Makanan favorit saya ialah " + myFavoritFoods[2]);
