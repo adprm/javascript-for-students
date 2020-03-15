ba7cbcae0d7e77ff6a11d1a7a9378b64a0875310
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 15:26:28 +0700

    JavaScript For Looping

---
+++ learn10.js
@@ -0,0 +1,12 @@
+// JavaScript For Looping
+for (i = 0; i <= 10; i++) {
+    document.write("Perulangan ke " + i + "<br>");
+}
+
+for (i = 1; i <= 50; i += 3) {
+    document.write("Ini looping kelipatan 3 " + i + "<br>");
+}
+
+for (i = 20; i >= 0; i--) {
+    document.write("Ini looping hitungan mundur" + i + "<br>");
+}
