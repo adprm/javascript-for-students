2b602df09c5ff4ccaf126278312ff0259061e86d
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-15 15:19:41 +0700

    JavaScript If

---
+++ learn3.js
@@ -0,0 +1,7 @@
+// JavaScript If
+var totalShopping = prompt("Masukkan total belanja anda : ", 0);
+
+if (totalShopping >= 100000) {
+    document.write("Selamat anda dapat hadiah karena belanja lebih dari 100.000<br>");
+}
+document.write("Terimakash telah berbelanja di toko kami")
