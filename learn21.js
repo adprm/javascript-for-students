f205dfceef8f26d2e5c9dc103fdd641f6c3d4501
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 07:09:23 +0700

    JavaScript Add Data Array

---
+++ learn21.js
@@ -0,0 +1,7 @@
+// JavaScript Add Data Array
+var products = ['Topi', 'Baju', 'Sepatu'];
+document.write("Data array sebelum di tambahkan <b>" + products + "</b><br>");
+
+// menambahkan data array
+products[3] = 'Celana';
+document.write("Data array setelah di tambahkan <b>" + products);
