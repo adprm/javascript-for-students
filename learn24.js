89f46eb848495b97686fe98b404a94a96f8b3a06
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 07:11:31 +0700

    JavaScript Delete Data Array with Method Pop

---
+++ learn24.js
@@ -0,0 +1,7 @@
+// JavaScript Delete Data Array with Method Pop
+var languages = ['JavaScript', 'PHP', 'Python', 'Java'];
+document.write("Data array sebelum dihapus dengan method pop = " + languages + "<br>");
+
+// menghapus data array dengan method pop
+languages.pop();
+document.write("Data array setelah dihapus = " + languages);
