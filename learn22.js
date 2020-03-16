199259f3ec4699af4a10c5eca957a351108b32f9
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 07:10:07 +0700

    JavaScript Add Data Array with Method Push

---
+++ learn22.js
@@ -0,0 +1,11 @@
+// JavaScript Add Data Array with Method Push
+var nameStudents = ['Adit', 'Dimas', 'Jean'];
+document.write("Data array sebelum di tambahkan dengan method push <b>" + nameStudents + "</b><br>");
+
+// menambahkan data array
+nameStudents.push('Arya');
+document.write("Setelah di tambahkan <b>" + nameStudents + "</b><br>");
+
+// dapat juga menambahkan data sekaligus
+nameStudents.push('Kevin', 'Ferdy', 'Ria');
+document.write("Menambahkan data array sekaligus <b>" + nameStudents)
