dc869efcb83a4e2e2529a276d00d42d1d0901e52
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 07:10:52 +0700

    JavaScript Delete Data Array

---
+++ learn23.js
@@ -0,0 +1,7 @@
+// JavaScript Delete Data Array
+var myCourses = ['Matematika', 'IPA', 'IPS', 'Bahasa'];
+document.write("Data array sebelum di hapus = " + myCourses + "<br>");
+
+// delete data array
+delete myCourses[2];
+document.write("Data array sesudah di hapus = " + myCourses);
