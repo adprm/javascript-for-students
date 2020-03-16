c5bdbe3d2d1bda16fb707cad94694e2d7e633ba1
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 14:12:04 +0700

    JavaScript Delete Data Array with Method Shift

---
+++ learn25.js
@@ -0,0 +1,7 @@
+// JavaScript Delete Data Array with Method Shift
+var nameCountries = ['Indonesia', 'Singapore', 'China', 'Jepang'];
+document.write("Sebelum data array dihapus " + nameCountries + "<br>");
+
+// delete data array with method shift
+nameCountries.shift();
+document.write("Data array sesudah dihapus " + nameCountries);
