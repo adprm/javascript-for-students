23067f46037d5651e52086c9999dd0986466650a
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 15:45:44 +0700

    JavaScript Array Method Includes

---
+++ learn29.js
@@ -0,0 +1,12 @@
+// JavaScript Array Method Includes
+var flowersName = ['melati', 'mawar', 'kamboja', 'raflesia'];
+
+// mencari data bunga mawar
+var thereRose = flowersName.includes('mawar');
+// pembuktian
+document.write(thereRose + "<br>");
+
+// mencari data bunga anggrek
+var thereOrchid = flowersName.includes('anggrek');
+// pembuktian
+document.write(thereOrchid);
