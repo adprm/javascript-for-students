b48778b1ca08b2f1e91054b22d1cb07d25affd10
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 14:13:38 +0700

    JavaScript Change Data Array

---
+++ learn27.js
@@ -0,0 +1,7 @@
+// JavaScript Change Data Array
+var products = ['Topi', 'Tas', 'Baju', 'Sepatu'];
+document.write("Data array sebelum di ubah " + products + "<br>");
+
+// change data array
+products[0] = 'Jaket';
+document.write("Data array setelah di ubah " + products);
