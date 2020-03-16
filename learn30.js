d94b77c2e4d7f23478ec608498b9ec8c913ddd1b
aditiyapermana <aditiyaprmn00@gmail.com>
2020-03-16 15:46:26 +0700

    JavaScript Array Method Sort

---
+++ learn30.js
@@ -0,0 +1,11 @@
+// JavaScript Array Method Sort
+var alphabets = ['a', 'd', 'b', 'c', 'e', 'g', 'h', 'f'];
+var numbers = [8, 7, 4, 3, 1, 2, 5, 9, 6];
+
+document.write("Data sebelum di sorting<br>");
+document.write(alphabets + "<br>");
+document.write(numbers + "<br>");
+
+document.write("Data setelah di sorting dengan method sort<br>");
+document.write(alphabets.sort() + "<br>");
+document.write(numbers.sort());
