8fe87dca416f4e4aa6502f26392ef059f51d1cdf
adprm <aditiyaprmn00@gmail.com>
2020-03-19 13:46:01 +0700

    JavaScript Return Value

---
+++ learn35.js
@@ -0,0 +1,11 @@
+// JavaScript Return Value
+var toward = (a, b) => {
+    result = a / b;
+    return result;
+}
+
+var value1 = 20;
+var value2 = 2;
+var resultToward = toward(value1, value2);
+
+alert(resultToward);
