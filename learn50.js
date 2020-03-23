d7f30a184704ca0bf3fe3358badb705b2839f73c
adprm <aditiyaprmn00@gmail.com>
2020-03-23 18:53:27 +0700

    JavaScript Math Max Min

---
+++ learn50.js
@@ -0,0 +1,13 @@
+// JavaScript Math Max Min
+// fungsi max mencari nilai terbesar
+var a = Math.max(10, 12, 1, 3, 13, 7, 8, 11);
+var b = Math.max(...[10, 9, 11, 12, 15, 20, 17]); //  Dalam bentuk array
+
+// fungsi min mencari nilai terkecil
+var c = Math.min(5, 6, 4, 3, 1, 2, 9);
+var d = Math.min(...[3, 4, 5, 7, 1, 0, 9]) //  Dalam bentuk array
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
