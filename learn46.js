a1c8af5c7696002f2d93006574eff75d4205224e
adprm <aditiyaprmn00@gmail.com>
2020-03-23 14:24:59 +0700

    JavaScript Math Logarithm-Rank-Exponential

---
+++ learn46.js
@@ -0,0 +1,8 @@
+// JavaScript Math Logarithm-Rank-Exponential
+var a = Math.log(10); //  fungsi logaritma
+var b = Math.pow(4, 2); //  fungsi pangkat
+var c = Math.exp(10); //  fungsi eksponensial
+
+console.log(a);
+console.log(b);
+console.log(c);
