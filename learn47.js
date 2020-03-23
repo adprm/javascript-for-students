005b6fecb2426854472bc1267e80041acfd60a64
adprm <aditiyaprmn00@gmail.com>
2020-03-23 14:25:56 +0700

    JavaScript Math Rounding up

---
+++ learn47.js
@@ -0,0 +1,18 @@
+// JavaScript Math Rounding up
+// pembulatan ke bawah
+var a = Math.floor(3.2);
+var b = Math.floor(3.7);
+// pembulatan ke angka terdekat
+var c = Math.round(3.5);
+var d = Math.round(5.3);
+// pembultan ke atas
+var e = Math.ceil(2.7);
+var f = Math.ceil(2.2);
+
+// cek hasil di console
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
