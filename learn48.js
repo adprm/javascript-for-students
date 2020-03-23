7bae82c604c253267fc65e90697593905441fc1c
adprm <aditiyaprmn00@gmail.com>
2020-03-23 18:52:15 +0700

    JavaScript Math Roo Function

---
+++ learn48.js
@@ -0,0 +1,13 @@
+// JavaScript Math Roo Function
+// fungsi akar kuadrat
+var a = Math.sqrt(25);
+var b = Math.sqrt(81);
+
+// fungsi akar kubik
+var c = Math.cbrt(125);
+var d = Math.cbrt(27);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
