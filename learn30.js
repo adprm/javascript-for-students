cb30bcd26d82def9c010318d16feff01283b5521
adprm <aditiyaprmn00@gmail.com>
2020-03-26 20:04:17 +0700

    JavaScript Array Method Sort

--- learn30.js
+++ learn30.js
@@ -2,10 +2,10 @@
 var alphabets = ['a', 'd', 'b', 'c', 'e', 'g', 'h', 'f'];
 var numbers = [8, 7, 4, 3, 1, 2, 5, 9, 6];
 
-document.write("Data sebelum di sorting<br>");
+document.write("Data before sorting<br>");
 document.write(alphabets + "<br>");
 document.write(numbers + "<br>");
 
-document.write("Data setelah di sorting dengan method sort<br>");
+document.write("Data after sorting using the sorting method<br>");
 document.write(alphabets.sort() + "<br>");
 document.write(numbers.sort());
