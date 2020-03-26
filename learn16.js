47994db8955856beffe2666c44dfd8796758f44c
adprm <aditiyaprmn00@gmail.com>
2020-03-26 16:34:22 +0700

    JavaScript While Nested

--- learn16.js
+++ learn16.js
@@ -1,10 +1,10 @@
 // JavaScript While Nested
-var ask = confirm("Apakan anda ingin mengulang ?");
+var ask = confirm("Do you want to repeat ?");
 var counter = 0;
 
 while (ask) {
     counter++;
     var star = "*".repeat(counter) + "<br>";
     document.write(counter + ":" + star);
-    ask = confirm("Ulangi ?");
+    ask = confirm("Repeat ?");
 }
