0487282cb713143357555cc65e41e09ad4406dd9
adprm <aditiyaprmn00@gmail.com>
2020-03-26 20:24:18 +0700

    JavaScript Function with Parameter

--- learn34.js
+++ learn34.js
@@ -1,10 +1,10 @@
 // JavaScript Function with Parameter
-// membuat fungsi dengan parameter
-// a dan b ini adalah senbuah parameter
+// create functions with parameters
+// a and b are parameters
 var count = (a, b) => {
     resultCount = a * b;
     alert("Hasil dari a * b = " + resultCount);
 }
 
-// kemudian memanggil parameter tersebut seperti ini
-count(2, 3); //  2 adalah a, dan 3 adalah b
+// then call the parameter like this
+count(2, 3); //  2 is a, and 3 is b
