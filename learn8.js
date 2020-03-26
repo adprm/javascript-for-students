35ce09689b40907f89b30b17b8953c3b80c26f6b
adprm <aditiyaprmn00@gmail.com>
2020-03-26 16:37:05 +0700

    JavaScript If Nested

--- learn8.js
+++ learn8.js
@@ -1,11 +1,11 @@
 // JavaScript If Nested
-var username = prompt("Masukkan username anda : ");
-var password = prompt("Masukkan password anda : ");
+var username = prompt("Enter your username : ");
+var password = prompt("Enter your password : ");
 
 if ((username.toUpperCase() == "ADIT")) {
     if ((password.toUpperCase() == "TIDA")) {
-        document.write("Anda berhasil login");
+        document.write("You have successfully logged in");
     } else {
-        document.write("Input tidak valid");
+        document.write("Invalid input");
     }
 }
