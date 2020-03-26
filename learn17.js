64b3531af0e6f806384952de7dc3ba6fa2886f16
adprm <aditiyaprmn00@gmail.com>
2020-03-26 19:59:04 +0700

    JavaScript Array Introduction

--- learn17.js
+++ learn17.js
@@ -1,9 +1,9 @@
 // JavaScript Array Introduction
 // Example array data 1
-var exampleArrayData = ['Pulpen', 'Pensil', 'Buku'];
+var exampleArrayData = ['Pen', 'Pencil', 'Book'];
 // Example array data 2
 var exampleArrayData2 = [15, 4.5, true, 'A', 'Indonesia'];
 // get array data
 // index array starts from 0
-exampleArrayData[2]; // Buku
+exampleArrayData[2]; // Book
 exampleArrayData2[1]; // 4.5
