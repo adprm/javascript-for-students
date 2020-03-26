9d0334e73c2ed7a622c9105a3b1e1fcd8e9100b6
adprm <aditiyaprmn00@gmail.com>
2020-03-26 20:01:33 +0700

    JavaScript Delete Data Array

--- learn23.js
+++ learn23.js
@@ -1,7 +1,7 @@
 // JavaScript Delete Data Array
-var myCourses = ['Matematika', 'IPA', 'IPS', 'Bahasa'];
-document.write("Data array sebelum di hapus = " + myCourses + "<br>");
+var myCourses = ['Mathematics', 'IPA', 'IPS', 'Language'];
+document.write("Data array before it is erased = " + myCourses + "<br>");
 
 // delete data array
 delete myCourses[2];
-document.write("Data array sesudah di hapus = " + myCourses);
+document.write("Data array after it is deleted = " + myCourses);
