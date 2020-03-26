44ef24652c74fe3fce366630e67465b41c97cd36
adprm <aditiyaprmn00@gmail.com>
2020-03-26 16:36:02 +0700

    JavaScript If Else If

--- learn5.js
+++ learn5.js
@@ -1,5 +1,5 @@
 // JavaScript If Else If
-var score = prompt("Masukkan nilai anda : ");
+var score = prompt("Enter your grades : ");
 var grade = "";
 if (score == 90) grade = "A";
 else if (score == 80) grade = "B";
@@ -7,4 +7,4 @@ else if (score == 70) grade = "C";
 else if (score == 60) grade = "D";
 else grade = "E";
 
-document.write("Nilai anda = " + score + " maka nilai mutu anda : " + grade);
+document.write("Your score = " + score + " then your quality value : " + grade);
