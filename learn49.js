b432db12538c095fd09ee91cf88094be1d26daea
adprm <aditiyaprmn00@gmail.com>
2020-03-27 10:47:07 +0700

    JavaScript Math Random

--- learn49.js
+++ learn49.js
@@ -1,10 +1,10 @@
 // JavaScript Math Random
-// fungsi math random mengahasilkan nilai acak dari 0.0 sampai 1.0
+// the random math function generates random values ​​from 0.0 to 1.0
 console.log(Math.random());
 console.log(Math.random());
 
-// membuat nilai acak dari range tertentu
-// dapat kita bungkus dengan fungsi
+// make a random value from a certain range
+// we can wrap it with functions
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
@@ -12,7 +12,7 @@ function getRndInteger(min, max) {
 console.log(getRndInteger(1, 100));
 console.log(getRndInteger(1, 5));
 
-// fungsi mutlak
+// absolute function
 var x = Math.abs(-12);
 var y = Math.abs(-100);
 
